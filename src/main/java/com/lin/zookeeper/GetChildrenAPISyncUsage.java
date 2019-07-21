17531c71a1ad41911f758b5c78330389bb7feef7
linhuang <1015550661@qq.com>
2019-07-21 20:44:04 +0800

    zookeeper

---
+++ src/main/java/com/lin/zookeeper/GetChildrenAPISyncUsage.java
@@ -0,0 +1,40 @@
+package com.lin.zookeeper;
+
+import org.apache.zookeeper.*;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+public class GetChildrenAPISyncUsage implements Watcher {
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+    private static ZooKeeper zk = null;
+
+    public static void main(String[] args) throws Exception {
+        String path = "/zk-book";
+        zk = new ZooKeeper("192.168.31.13:2181", 5000, new GetChildrenAPISyncUsage());
+        connectedSemaphore.await();
+        zk.create(path, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        zk.create(path + "/c1", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+
+        List<String> childrenList = zk.getChildren(path, true);
+        System.out.println(childrenList);
+
+        zk.create(path + "/c2", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            if (Event.EventType.None == watchedEvent.getType() && null == watchedEvent.getPath()) {
+                connectedSemaphore.countDown();
+            } else if (watchedEvent.getType() == Event.EventType.NodeChildrenChanged) {
+                try {
+                    System.out.println("ReGet Child:" + zk.getChildren(watchedEvent.getPath(), true));
+                } catch (Exception ignored) {}
+            }
+        }
+    }
+}
